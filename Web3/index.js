c53415f4d2b473584a6d6177c381c590a7943c57
leiyin3211 <leiyin321@163.com>
2021-10-10 20:12:59 +0800

    更新前端

--- Web3/index.js
+++ Web3/index.js
@@ -2,9 +2,10 @@ import Web3 from 'web3'
 import cubeAbi from '../abi/CubeRouter.json'
 import erc20abi from '../abi/ERC20.json'
 
-const hecoAddr = '0x867E9c9a4d3cbc772397eda69108e43C1B0D1669'
-const oecAddr = '0xe444b78c2037da51f56C217CDdBdda45b2416aaf'
-console.log('abi', cubeAbi)
+const hecoAddr = '0xA1807b02EA107045d1C5Ef868294881Cd9b20A6c'
+const oecAddr = '0x17971AC1D2830C1AB84a2259585436c5c69C6E4d'
+const ethAddr = '0xbe0b2ab4b7b178040976ee21bd86160E5CEDA34d'
+
 
 // heco
 const HecoProvider = new Web3(new Web3.providers.HttpProvider('https://http-testnet.hecochain.com'))
@@ -14,6 +15,10 @@ const HecoContract = new HecoProvider.eth.Contract(cubeAbi, hecoAddr)
 const OecProvider = new Web3(new Web3.providers.HttpProvider('https://exchainrpc.okex.org'))
 const OecContract = new OecProvider.eth.Contract(cubeAbi, oecAddr)
 
+//eth
+const EthProvider = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'))
+const EthContract = new EthProvider.eth.Contract(cubeAbi, oecAddr)
 
 
-export { HecoContract, HecoProvider, OecContract, OecProvider, erc20abi, hecoAddr, oecAddr }
+
+export { HecoContract, HecoProvider, OecContract, OecProvider, erc20abi, hecoAddr, oecAddr, ethAddr, EthProvider, EthContract }
